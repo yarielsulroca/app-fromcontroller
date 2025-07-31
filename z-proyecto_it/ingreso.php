7c1c2a1127f9152e133140751a1d69487e93f721
Yariel Sulroca <sulroca@gmail.com>
2025-07-31 20:47:11 -0300

    Includo Proyecoto Inicial de IT para generar Nuevo ejercicio

---
+++ z-proyecto_it/ingreso.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>ComercioIT | Tu E-Shop en PHP</title>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!--theme-style-->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+	<!--//theme-style-->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!--fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<!--//fonts-->
+	<script src="js/jquery.min.js"></script>
+	<!--script-->
+	</head>
+	<body> 
+		<!--header-->
+		<div class="header">
+			<div class="bottom-header">
+				<div class="container">
+					<div class="header-bottom-left">
+						<div class="logo"><a href="index.php">Comercio<strong>IT</strong></a></div>
+						<!--div class="search">
+							<input type="text" name="q">
+							<input type="submit" value="BUSCAR">
+						</div-->
+						<div class="clearfix"></div>
+					</div>
+					<div class="header-bottom-right">					
+						<!--div class="account">
+							<a href="ingreso.html"><span></span> TU CUENTA</a>
+						</div-->
+						<ul class="login">
+							<li><a href="ingreso.php"><span></span> INGRESAR</a></li>
+							&nbsp;|&nbsp;
+							<li><a href="registro.php">REGISTRARME</a></li>
+							&nbsp;|&nbsp;
+							<li><a href="contacto.php">CONTACTO</a></li>
+						</ul>
+						<!--div class="cart"><a href="#"><span></span>CART</a></div-->
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>	
+				</div>
+			</div>
+		</div>
+		<!---->
+		<div class="container">
+			<section id="page">
+				<div class="account_grid">
+	<div class="login-right">
+		<h3>INGRESO DE USUARIO</h3>
+		<form action="#" method="post">
+		<div>
+			<span>E-Mail:</span>
+			<input type="text" name="email"> 
+		</div>
+		<div>
+			<span>Contraseña:</span>
+			<input type="password" name="pass"> 
+		</div>
+			<input type="submit" value="Ingresar">
+			<br>
+			<a class="forgot" href="#">¿Olvidaste tu contraseña?</a>
+		</form>
+	</div>	
+	<div class=" login-left">
+		<h3>¿NUEVO USUARIO?</h3>
+		<a class="acount-btn" href="registro.php">Crear una cuenta</a>
+	</div>
+	<div class="clearfix"></div>
+</div>
+			</section>
+			<div class="clearfix"></div>
+		</div>
+
+		<!---->
+		<div class="footer">
+			<div class="footer-top">
+				<div class="container">
+					<div class="latter">
+						<h6>LA MEJOR MANERA DE COMPRAR ONLINE!</h6>
+						<div class="clearfix"></div>
+					</div>
+					<div class="latter-right">
+						<p>SEGUINOS</p>
+						<ul class="face-in-to">
+							<li><a href="#"><span></span></a></li>
+							<li><a href="#"><span class="facebook-in"></span></a></li>
+							<div class="clearfix"></div>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="footer-bottom">
+				<div class="container">
+					<div class="footer-bottom-cate cate-bottom">
+						<h6>DIRECCIÓN</h6>
+						<ul>
+							<li>Lavalle 648, 8° Piso</li>
+							<li>C.A.B.A.</li>
+							<li>Buenos Aires</li>
+							<li>Argentina</li>
+							<li class="phone">(011) 4328-0457</li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<!--initiate accordion-->
+		<script src="js/custom.js"></script>
+	</body>
+</html>
---
+++ z-proyecto_it/producto.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>ComercioIT | Tu E-Shop en PHP</title>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!--theme-style-->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+	<!--//theme-style-->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!--fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<!--//fonts-->
+	<script src="js/jquery.min.js"></script>
+	<!--script-->
+	</head>
+	<body> 
+		<!--header-->
+		<div class="header">
+			<div class="bottom-header">
+				<div class="container">
+					<div class="header-bottom-left">
+						<div class="logo"><a href="index.php">Comercio<strong>IT</strong></a></div>
+						<!--div class="search">
+							<input type="text" name="q">
+							<input type="submit" value="BUSCAR">
+						</div-->
+						<div class="clearfix"></div>
+					</div>
+					<div class="header-bottom-right">					
+						<!--div class="account">
+							<a href="ingreso.html"><span></span> TU CUENTA</a>
+						</div-->
+						<ul class="login">
+							<li><a href="ingreso.php"><span></span> INGRESAR</a></li>
+							&nbsp;|&nbsp;
+							<li><a href="registro.php">REGISTRARME</a></li>
+							&nbsp;|&nbsp;
+							<li><a href="contacto.php">CONTACTO</a></li>
+						</ul>
+						<!--div class="cart"><a href="#"><span></span>CART</a></div-->
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>	
+				</div>
+			</div>
+		</div>
+		<!---->
+		<div class="container">
+			<section id="page">
+				<div class="single_top">
+	<div class="single_grid">
+		<div class="grid images_3_of_2">
+			<ul id="etalage">
+				<li>
+					<img class="etalage_thumb_image" src="images/productos/P001.jpg" class="img-responsive" />
+				</li>
+			</ul>
+			<div class="clearfix"></div>		
+		</div>
+		<div class="desc1 span_3_of_2">
+			<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+			<div class="cart-b">
+				<div class="left-n ">$329.58</div>
+				<a class="now-get get-cart-in" href="#">COMPRAR</a> 
+				<div class="clearfix"></div>
+			</div>
+			<h6>100 unid. en stock</h6>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			<div class="share">
+				<h5>Compartir Producto:</h5>
+				<ul class="share_nav">
+					<li><a href="#"><img src="images/facebook.png" title="facebook"></a></li>
+					<li><a href="#"><img src="images/twitter.png" title="Twiiter"></a></li>
+					<li><a href="#"><img src="images/rss.png" title="Rss"></a></li>
+					<li><a href="#"><img src="images/gpluse.png" title="Google+"></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+			</section>
+			<div class="clearfix"></div>
+		</div>
+
+		<!---->
+		<div class="footer">
+			<div class="footer-top">
+				<div class="container">
+					<div class="latter">
+						<h6>LA MEJOR MANERA DE COMPRAR ONLINE!</h6>
+						<div class="clearfix"></div>
+					</div>
+					<div class="latter-right">
+						<p>SEGUINOS</p>
+						<ul class="face-in-to">
+							<li><a href="#"><span></span></a></li>
+							<li><a href="#"><span class="facebook-in"></span></a></li>
+							<div class="clearfix"></div>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="footer-bottom">
+				<div class="container">
+					<div class="footer-bottom-cate cate-bottom">
+						<h6>DIRECCIÓN</h6>
+						<ul>
+							<li>Lavalle 648, 8° Piso</li>
+							<li>C.A.B.A.</li>
+							<li>Buenos Aires</li>
+							<li>Argentina</li>
+							<li class="phone">(011) 4328-0457</li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<!--initiate accordion-->
+		<script src="js/custom.js"></script>
+	</body>
+</html>
