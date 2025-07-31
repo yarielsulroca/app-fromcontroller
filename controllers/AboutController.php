539f9b8ba0101cf038ad8e54dcdbaeabf80c9fce
Yariel Sulroca <sulroca@gmail.com>
2025-07-31 20:30:59 -0300

    Vista Home y About + Controladores actulizados con [],

--- controllers/AboutController.php
+++ controllers/AboutController.php
@@ -8,16 +8,27 @@ class AboutController {
   }
     // Método para manejar la solicitud de la página "Acerca de"
   public function index() {
-    $this->layout->setTitle('Inicio - Mi Sitio Web');
-    $this->layout->setMetaDescription('Bienvenido a nuestro sitio web. Descubre nuestros servicios y productos.');
-    $this->layout->addStyle('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">');
-    //$this->layout->addScript('<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>');
-
-
-    $this->layout->render('about', [
-        'pageTitle' => 'Bienvenido a Acerda de '
+    $this->layout->setTitle('Acerca de - Mi Sitio Web');
+        $this->layout->setMetaDescription('Conoce más sobre nuestra empresa, misión, visión y valores.');
         
-    ]);
+        $this->layout->render('about', [
+            'pageTitle' => 'Acerca de Nosotros',
+            'companyInfo' => [
+                'name' => 'Mi Empresa S.A.',
+                'founded' => '2020',
+                'employees' => '50+',
+                'clients' => '200+'
+            ],
+            'mission' => 'Proporcionar soluciones tecnológicas innovadoras que impulsen el crecimiento de nuestros clientes.',
+            'vision' => 'Ser líderes en el desarrollo de soluciones digitales que transformen la manera de hacer negocios.',
+            'values' => [
+                'Innovación',
+                'Calidad',
+                'Integridad',
+                'Colaboración',
+                'Excelencia'
+            ]
+        ]);
     }
 
 }
