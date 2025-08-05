c58aa520fc420f9b293462b5e4e0ad09de921bb7
Yariel Sulroca <sulroca@gmail.com>
2025-08-05 20:30:34 -0300

    config + databece completado

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+// Configuración general de la aplicación
+return [
+    'name' => 'Curso EducacionIT',
+    'version' => '1.0.0',
+    'debug' => true,
+    'timezone' => 'America/Argentina/Buenos_Aires',
+    'base_url' => 'http://app-fromcontroller.test',
+    'upload_path' => __DIR__ . '/../public/uploads/',
+    'max_file_size' => 10 * 1024 * 1024,
+];
